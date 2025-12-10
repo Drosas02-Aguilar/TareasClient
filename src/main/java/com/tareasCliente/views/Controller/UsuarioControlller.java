4d15f85333941a993bed8e032afa877dd16a3338
elven <elven@Damian>
2025-12-09 22:39:58 -0600

    Cambios en front

--- src/main/java/com/tareasCliente/views/Controller/UsuarioControlller.java
+++ src/main/java/com/tareasCliente/views/Controller/UsuarioControlller.java
@@ -65,99 +65,144 @@ public class UsuarioControlller {
             return "redirect:/auth/login";
         }
     }
-
+    
     @GetMapping("/editar")
-    public String FormularioEditar(Model model, HttpSession session) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        String username = (String) session.getAttribute("username");
-        Integer idUsuario = (Integer) session.getAttribute("idUsuario");
-
-        if (username == null || idUsuario == null) {
-            model.addAttribute("error", "Debes iniciar sesión primero");
-            model.addAttribute("usuario", new Usuario());
-            return "login";
-        }
-
-        try {
-            String token = (String) session.getAttribute("token");
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(token);
-            HttpEntity<?> request = new HttpEntity<>(headers);
-
-            ResponseEntity<Result<Usuario>> response = restTemplate.exchange(
-                    "http://localhost:8080/api/usuario/" + idUsuario,
-                    HttpMethod.GET,
-                    request,
-                    new ParameterizedTypeReference<Result<Usuario>>() {
-            }
-            );
-
-            Result<Usuario> result = response.getBody();
-
-            if (response.getStatusCode().is2xxSuccessful() && result != null && result.object != null) {
-                Usuario usuario = result.object;
-                model.addAttribute("usuario", usuario);
-                model.addAttribute("username", username);
-                return "usuario/editar";
-
-            } else {
-                model.addAttribute("error", "Usuario no encontrado");
-                return "redirect:/usuario/perfil";
-            }
-
-        } catch (Exception ex) {
-            model.addAttribute("error", "Error al cargar datos: " + ex.getMessage());
-            return "redirect:/usuario/perfil";
-        }
-    }
-    
-    @PostMapping("/editar")
-    public String actualizarUsuario(@ModelAttribute Usuario usuario, HttpSession session, Model model){
+public String FormularioEditar(Model model, HttpSession session) {
     RestTemplate restTemplate = new RestTemplate();
-    
+
     String username = (String) session.getAttribute("username");
-    Integer idUsuario = (Integer) session.getAttribute("idUsername");
-    
-    if(username == null || idUsuario == null){
-    
-    model.addAttribute("error", "Debes iniciar sesion primero");
-    model.addAttribute("usuario", new Usuario());
+    Integer idUsuario = (Integer) session.getAttribute("idUsuario");
+
+    if (username == null || idUsuario == null) {
+        model.addAttribute("error", "Debes iniciar sesión primero");
+        model.addAttribute("usuario", new Usuario());
+        return "login";
     }
-    
-    try{
+
+    try {
         String token = (String) session.getAttribute("token");
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(token);
-        
-        HttpEntity<Usuario> request = new HttpEntity<>(usuario,headers);
-        
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
         ResponseEntity<Result<Usuario>> response = restTemplate.exchange(
-        "http://localhost:8080/api/usuario/actualizar/" + idUsuario,
-                HttpMethod.PUT,
+                "http://localhost:8080/api/usuario/" + idUsuario,
+                HttpMethod.GET,
                 request,
-                new ParameterizedTypeReference<Result<Usuario>>(){}
+                new ParameterizedTypeReference<Result<Usuario>>() {}
         );
+
         Result<Usuario> result = response.getBody();
-        
-        if(response.getStatusCode().is2xxSuccessful() && result != null){
-            session.setAttribute("username", result.object.getUsername());
-        model.addAttribute("success", "Usuario actualizado exitosamente");
-        return "redirect:/usuario/perfil";
-        }else{
-        model.addAttribute("error", result !=null ? result.errorMessage : "Error al actualizar usuario"); 
+
+        if (response.getStatusCode().is2xxSuccessful() && result != null && result.object != null) {
+            Usuario usuario = result.object;
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("idUsuario", idUsuario);
+            model.addAttribute("username", username);
+            return "usuario/editar";
+        } else {
+            model.addAttribute("error", "Usuario no encontrado");
+            return "redirect:/usuario/perfil";
         }
-        
-    }catch(Exception ex){
-                model.addAttribute("error", "Error al actualizar usuario: " + ex.getMessage());
+
+    } catch (Exception ex) {
+        model.addAttribute("error", "Error al cargar datos: " + ex.getMessage());
+        return "redirect:/usuario/perfil";
     }
+}
+
+//    @GetMapping("/editar")
+//    public String FormularioEditar(Model model, HttpSession session) {
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        String username = (String) session.getAttribute("username");
+//        Integer idUsuario = (Integer) session.getAttribute("idUsuario");
+//
+//        if (username == null || idUsuario == null) {
+//            model.addAttribute("error", "Debes iniciar sesión primero");
+//            model.addAttribute("usuario", new Usuario());
+//            return "login";
+//        }
+//
+//        try {
+//            String token = (String) session.getAttribute("token");
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setBearerAuth(token);
+//            HttpEntity<?> request = new HttpEntity<>(headers);
+//
+//            ResponseEntity<Result<Usuario>> response = restTemplate.exchange(
+//                    "http://localhost:8080/api/usuario/" + idUsuario,
+//                    HttpMethod.GET,
+//                    request,
+//                    new ParameterizedTypeReference<Result<Usuario>>() {
+//            }
+//            );
+//
+//            Result<Usuario> result = response.getBody();
+//
+//            if (response.getStatusCode().is2xxSuccessful() && result != null && result.object != null) {
+//                Usuario usuario = result.object;
+//                model.addAttribute("usuario", usuario);
+//                model.addAttribute("username", username);
+//                return "usuario/editar";
+//
+//            } else {
+//                model.addAttribute("error", "Usuario no encontrado");
+//                return "redirect:/usuario/perfil";
+//            }
+//
+//        } catch (Exception ex) {
+//            model.addAttribute("error", "Error al cargar datos: " + ex.getMessage());
+//            return "redirect:/usuario/perfil";
+//        }
+//    }
     
-    
-    model.addAttribute("usuario", usuario);
-    model.addAttribute("username",username);
-    return "usuario/editar";
-    }
+//    @PostMapping("/editar")
+//    public String actualizarUsuario(@ModelAttribute Usuario usuario, HttpSession session, Model model){
+//    RestTemplate restTemplate = new RestTemplate();
+//    
+//    String username = (String) session.getAttribute("username");
+//    Integer idUsuario = (Integer) session.getAttribute("idUsername");
+//    
+//    if(username == null || idUsuario == null){
+//    
+//    model.addAttribute("error", "Debes iniciar sesion primero");
+//    model.addAttribute("usuario", new Usuario());
+//    }
+//    
+//    try{
+//        String token = (String) session.getAttribute("token");
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.setBearerAuth(token);
+//        
+//        HttpEntity<Usuario> request = new HttpEntity<>(usuario,headers);
+//        
+//        ResponseEntity<Result<Usuario>> response = restTemplate.exchange(
+//        "http://localhost:8080/api/usuario/actualizar/" + idUsuario,
+//                HttpMethod.PUT,
+//                request,
+//                new ParameterizedTypeReference<Result<Usuario>>(){}
+//        );
+//        Result<Usuario> result = response.getBody();
+//        
+//        if(response.getStatusCode().is2xxSuccessful() && result != null){
+//            session.setAttribute("username", result.object.getUsername());
+//        model.addAttribute("success", "Usuario actualizado exitosamente");
+//        return "redirect:/usuario/perfil";
+//        }else{
+//        model.addAttribute("error", result !=null ? result.errorMessage : "Error al actualizar usuario"); 
+//        }
+//        
+//    }catch(Exception ex){
+//                model.addAttribute("error", "Error al actualizar usuario: " + ex.getMessage());
+//    }
+//    
+//    
+//    model.addAttribute("usuario", usuario);
+//    model.addAttribute("username",username);
+//    return "usuario/editar";
+//    }
     
     @PostMapping("/eliminar")
     public String eliminarUsuario(HttpSession session, Model model) {
